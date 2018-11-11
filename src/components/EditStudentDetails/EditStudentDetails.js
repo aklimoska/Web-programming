241ed26dd79c776202f87a951ea0336e70f558df
Mario Krstevski <mariokrstevski@hotmail.com>
2018-11-12 00:33:28 +0100

    New students is successfully updated from inputs

--- src/components/EditStudentDetails/EditStudentDetails.js
+++ src/components/EditStudentDetails/EditStudentDetails.js
@@ -5,28 +5,42 @@ class EditStudentDetails extends Component {
     constructor(props) {
         super(props);
 
-        console.log(this.props.selectedStudent);
 
     }
-    render() {
-        let newStudent = {
-            studentName: '',
-            studentSurname: '',
-            index: '',
-            program: ''
-        }
 
+
+    render() {
+
+        let newStudent;
+
+        const setStudent = (formSubmitEvent) => {
+            formSubmitEvent.preventDefault();
+
+            newStudent = {
+                studentName: formSubmitEvent.target.name.value,
+                studentSurname: formSubmitEvent.target.surname.value,
+                index: formSubmitEvent.target.index.value,
+                program: formSubmitEvent.target.nasoka.value
+            };
+
+            console.log("test", newStudent);
+            onClick();
+        };
+
+        const onClick = () => { this.props.apply(newStudent) }
 
         return (
             <div className="row">
-                <div className="col-2"><label htmlFor="name">Name: </label><input type="text" name="name" id="name" />
-                </div>
-                <div className="col-2"><label htmlFor="surname" >Surname: </label><input type="text" name="surname" id="surname" /></div>
-                <div className="col-2"><label htmlFor="index">Index: </label><input type="text" name="index" id="index" /></div>
-                <div className="col-2"><label htmlFor="nasoka">Nasoka: </label><input type="text" name="nasoka" id="nasoka" /></div>
 
-                <div className="col-1"> <button onClick={() => this.props.apply(newStudent)}> <i className="fa fa-paper-plane"></i> </button> </div>
+                <form onSubmit={setStudent}>
+                    <div className="col-2"><label htmlFor="name">Name: </label><input type="text" name="name" id="name" />
+                    </div>
+                    <div className="col-2"><label htmlFor="surname" >Surname: </label><input type="text" name="surname" id="surname" /></div>
+                    <div className="col-2"><label htmlFor="index">Index: </label><input type="text" name="index" id="index" /></div>
+                    <div className="col-2"><label htmlFor="nasoka">Nasoka: </label><input type="text" name="nasoka" id="nasoka" /></div>
 
+                    <div className="col-1"> <button type="submit"> <i className="fa fa-paper-plane"></i> </button> </div>
+                </form>
 
             </div >
         );
